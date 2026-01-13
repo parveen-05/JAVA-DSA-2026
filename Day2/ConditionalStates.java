45fc92bf3dc802102edbff8a7d71a4c37891e21c
parveen-05 <parveenrt05@gmail.com>
2026-01-13 22:43:10 +0530

    Update ConditionalStates.java

--- Day2/ConditionalStates.java
+++ Day2/ConditionalStates.java
@@ -1,1 +1,17 @@
+package day1;
+import java.util.*;
+public class ConditionalStates {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the age : ");
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+
+    }
+}
 
