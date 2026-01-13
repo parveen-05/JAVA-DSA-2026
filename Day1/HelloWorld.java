fe45188c7ce61668191cdccaeb43a522a58f85fa
parveen-05 <parveenrt05@gmail.com>
2026-01-13 18:23:06 +0530

    Update HelloWorld.java

--- Day1/HelloWorld.java
+++ Day1/HelloWorld.java
@@ -1,5 +1,7 @@
-public class HelloWorld{
-  public static void main (String arg[]){
-    System.out.println("Fresh Start Day 1 JAVA HelloWorld");
-      }
+package day1;
+
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
 }
