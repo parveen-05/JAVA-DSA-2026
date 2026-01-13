3e52f5385553ce9e9b0d79b396754c7bfe806271
parveen-05 <parveenrt05@gmail.com>
2026-01-13 22:44:17 +0530

    Create Odd_or_Even.java

---
+++ Day2/Odd_or_Even.java
@@ -0,0 +1,19 @@
+package day1;
+
+import java.util.*;
+
+public class Odd_OR_Even {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+
+    }
+}
