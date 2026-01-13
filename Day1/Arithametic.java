2ba66a87ba00362183e6de7b6f1ee5807ff6c696
parveen-05 <parveenrt05@gmail.com>
2026-01-13 18:26:28 +0530

    Create Arithametic.java

---
+++ Day1/Arithametic.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Arithamatic {
+    public static void main(String args[]){
+        int a = 10;
+         int b = 5;
+         int sum = a + b;
+         System.out.println(sum);
+         int diff = a - b;
+        System.out.println(diff);
+
+        int mul = a*b;
+        System.out.println(mul);
+    }
+}
