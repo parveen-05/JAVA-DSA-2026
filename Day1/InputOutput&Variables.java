78ad3437c7f9f43ef3dd0b1db3db60582a104ac6
parveen-05 <parveenrt05@gmail.com>
2026-01-13 18:27:48 +0530

    Create InputOutput&Variables.java

---
+++ Day1/InputOutput&Variables.java
@@ -0,0 +1,19 @@
+package day1;
+
+import java.sql.SQLOutput;
+import java.util.*;
+public class Qs1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter variable a : ");
+
+        int a = sc.nextInt();
+        System.out.print("enter variable b : ");
+        int b = sc.nextInt();
+
+        int sum = a+b;
+        System.out.print("Sum is : ");
+        System.out.println(sum);
+
+    }
+}
