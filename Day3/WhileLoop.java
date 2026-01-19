94325af34c73d15986e5f622fe47b940d4f21232
parveen-05 <parveenrt05@gmail.com>
2026-01-19 18:07:37 +0530

    Create WhileLoop.java

---
+++ Day3/WhileLoop.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class WhileLoop {
+    public static void main(String args[]){
+        int i=0;
+        while(i<11){
+            System.out.println("Parveen");
+            i=i+1;
+        }
+        int j=0;
+        while(j<6){
+            System.out.println(j);
+            j=j+1;
+        }
+    }
+}
