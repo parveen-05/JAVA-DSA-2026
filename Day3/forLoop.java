15bb9c837df800c7dd9a4adf26417f779c5d8b1a
parveen-05 <parveenrt05@gmail.com>
2026-01-19 18:06:58 +0530

    Create forLoop.java

---
+++ Day3/forLoop.java
@@ -0,0 +1,13 @@
+package day3;
+import java.util.*;
+
+public class forLoop {
+    public static void main(String args[]){
+        for(int a=0; a<3;a=a+1){
+            System.out.println("HELLO");
+        }
+        for(int b=0; b<10; b=b+1) {
+            System.out.println("Parveen");
+        }
+    }
+}
