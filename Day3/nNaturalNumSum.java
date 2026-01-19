09b52e92c6b351a24c074da3fe24f5b547c5862a
parveen-05 <parveenrt05@gmail.com>
2026-01-19 18:08:45 +0530

    Create nNaturalNumSum.java

---
+++ Day3/nNaturalNumSum.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class NaturalNumSum {
+    public static void main(String args[]){
+        int sum = 0;
+        for(int i=1; i<=4; i++){
+            sum = sum +i;
+        }
+        System.out.println(sum);
+    }
+}
