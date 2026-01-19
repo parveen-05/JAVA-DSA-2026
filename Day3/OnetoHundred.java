569c13ead3a0dd516cc719eb7342778326ee31b5
parveen-05 <parveenrt05@gmail.com>
2026-01-19 18:10:19 +0530

    Create OnetoHundred.java

---
+++ Day3/OnetoHundred.java
@@ -0,0 +1,12 @@
+package day3;
+
+public class oneTohundred {
+    public static void main(String args[]){
+        for(int i=0; i<101;i++){
+            System.out.print(i+" ");
+        }
+        for(int j = 0; j<101;j++){
+            System.out.println(j);
+        }
+    }
+}
