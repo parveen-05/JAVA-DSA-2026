7ed4e261d904f96de6e128bddda11f1a255ae7e6
parveen-05 <parveenrt05@gmail.com>
2026-01-19 18:08:15 +0530

    Create DoWhileLoop.java

---
+++ Day3/DoWhileLoop.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class DoWhileLoop {
+    public static void main(String args[]){
+        int i = 0;
+        do{
+            System.out.println("HI");
+            i=i+1;
+        } while(i<5);
+
+        int j=12;
+        do{
+            System.out.println("Parveen");
+        } while(j<3);
+    }
+}
