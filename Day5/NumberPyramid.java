9845925aff1ed21a59131ebd87675617b60aef33
parveen-05 <parveenrt05@gmail.com>
2026-01-22 00:20:27 +0530

    Create NumberPyramid.java

---
+++ Day5/NumberPyramid.java
@@ -0,0 +1,17 @@
+package day5;
+
+public class NumberPyramid {
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            int spaces=n-i;
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
