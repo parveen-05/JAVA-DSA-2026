b97808242d3c089e6232b55d13da83260bc76eb9
parveen-05 <parveenrt05@gmail.com>
2026-01-22 12:30:42 +0530

    Create CalculateSum.java

---
+++ Day6/CalculateSum.java
@@ -0,0 +1,16 @@
+package day6;
+import java.util.*;
+public class CalculateSum {
+    public static int printSum(int a, int b){
+        int sum = a + b;
+       return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+       int sum = printSum(a,b);
+        System.out.println(sum);
+    }
+}
