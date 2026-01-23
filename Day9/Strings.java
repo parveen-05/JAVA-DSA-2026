89c6f78bbf8cfad618fbfe647ea9cc687ab7b3f8
parveen-05 <parveenrt05@gmail.com>
2026-01-23 19:55:44 +0530

    Create Strings.java

---
+++ Day9/Strings.java
@@ -0,0 +1,15 @@
+package day9;
+import java.util.*;
+public class Strings {
+    public static void main(String args[]){
+        //string declaration
+        String name = "Parveen";
+        String full_name = "Parveen R T. ";
+        //take input
+        Scanner sc = new Scanner(System.in);
+       // String Name = sc.next();
+       // System.out.println("Your name is : "+Name);
+        String Name1 = sc.nextLine();
+        System.out.println("Your full name is : "+Name1);
+    }
+}
