f2b25e75bb10ce7cc7f04435e685680b4c91524b
parveen-05 <parveenrt05@gmail.com>
2026-01-23 19:57:03 +0530

    Create SubStrings.java

---
+++ Day9/SubStrings.java
@@ -0,0 +1,11 @@
+package day9;
+
+public class SubStrings {
+    public static void main(String args[]){
+        String sentence = "Parveen";
+        //String name = sentence.substring(0, 5);
+        //String name = sentence.substring(3);
+        String name = sentence.substring(1, 5);
+        System.out.println(name);
+    }
+}
