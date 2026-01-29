b5a6dafb3bf10b037f11367795fa3e9b3d831785
parveen-05 <parveenrt05@gmail.com>
2026-01-29 13:18:12 +0530

    Create xPownLogn.java

---
+++ Day12/xPownLogn.java
@@ -0,0 +1,28 @@
+package day12;
+import java.util.Scanner;
+public class xPownLogn {
+    public static int printPow(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        if(x==0){
+            return 0;
+        }
+        if(n%2==0){
+            return printPow(x,n/2) * printPow(x,n/2);
+        }
+        else{
+            return printPow(x,n/2) * printPow(x,n/2)*x;
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number x : ");
+        int x = sc.nextInt();
+        System.out.print("Enter number n : ");
+        int n = sc.nextInt();
+
+        int ans = printPow(x, n);
+        System.out.println(ans);
+    }
+    }
