c0ba50d2f02665a44b20fa572b572959154c0fbc
parveen-05 <parveenrt05@gmail.com>
2026-01-29 13:17:24 +0530

    Create Fibonacci.java

---
+++ Day12/Fibonacci.java
@@ -0,0 +1,21 @@
+package day12;
+import java.util.*;
+public class Fibonacci {
+    public static void printFib(int a, int b,int n){
+        if(n==0){
+            return;
+        }
+        int c= a+b;
+        System.out.println(c);
+        printFib(b,c,n-1);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        int a= 0;int b=1;
+        System.out.println(a);
+        System.out.println(b);
+        printFib(a,b,n-2);
+    }
+}
