6b7d7a604f186ebdcd55b1bcdfac00b0f7482b82
parveen-05 <parveenrt05@gmail.com>
2026-02-12 15:45:56 +0530

    Create subSet.java

---
+++ Day14/subSet.java
@@ -0,0 +1,29 @@
+package day14;
+import java.util.*;
+public class subSet {
+    public static void printSubset(ArrayList<Integer> subset){
+        for(int i=-0;i<subset.size();i++){
+            System.out.print(subset.get(i)+" ");
+        }
+        System.out.println();
+    }
+    public static void findSubset(int n, ArrayList<Integer> subset){
+        if(n==0){
+            printSubset(subset);
+            return;
+        }
+        //get add
+        subset.add(n);
+        findSubset(n-1, subset);
+        //not gonnna add
+        subset.remove(subset.size()-1);
+        findSubset(n-1, subset);
+
+    }
+
+    public static void main(String args[]){
+        int n=3;
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubset(n, subset);
+    }
+}
