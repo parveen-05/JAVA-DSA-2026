42d2a24b39c4f5d036ffe54819e5377cf6793a6f
parveen-05 <parveenrt05@gmail.com>
2026-06-16 12:44:49 +0530

    Create Push@Bottom_of_Stack.java

---
+++ Day22/Stack_Data_Structure/Push@Bottom_of_Stack.java
@@ -0,0 +1,30 @@
+package day22;
+import java.util.*;
+
+public class Push_at_Bottom_of_Stack {
+    public static void pushAtBottom(int data, Stack<Integer> s){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+
+
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtBottom(4, s);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
