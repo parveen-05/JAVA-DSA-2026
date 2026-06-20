a2356183152c17ec90e5055a5bd391fcf7551a10
parveen-05 <parveenrt05@gmail.com>
2026-06-20 18:46:58 +0530

    Update Subarray_sum_equal_to_K.java

--- Day28/Hashing_Problems/Subarray_sum_equal_to_K.java
+++ Day28/Hashing_Problems/Subarray_sum_equal_to_K.java
@@ -1,1 +1,28 @@
+package day27;
+import java.util.*;
+
+public class Subarray_Sum_Equals2_K {
+    public static void main(String[] args) {
+        int arr[]={10,2, -2, -20, 10};
+        int k = -10;
+        HashMap<Integer, Integer> map = new HashMap<>();//<sum, freq>
+
+        map.put(0, 1);//empty subarray
+        int ans=0;
+        int sum=0;
+        for(int j=0; j<arr.length;j++){
+            sum += arr[j];
+
+            if(map.containsKey(sum-k)){
+                ans += map.get(sum-k);
+            }
+            if(map.containsKey(sum)){
+                map.put(sum, map.get(sum)+1);
+            }else{
+                map.put(sum, 1);
+            }
+        }
+        System.out.println(ans);
+    }
+}
 
