0f8c6b2521a5a7576b8f14ecfa513fcd2025b00b
parveen-05 <parveenrt05@gmail.com>
2026-06-20 20:44:34 +0530

    Update and rename Prob1.java to Insert_In_Trie.java

---
+++ Day29/Trie_Data_Structure/Insert_In_Trie.java
@@ -0,0 +1,38 @@
+package day28;
+
+public class Trie_Data_Structure {
+    static class Node{
+        Node [] children;
+        boolean eow;
+
+        public Node(){
+            children = new Node[26];//a to z
+            for(int i=0;i<26;i++){
+                children[i] = null;
+            }
+            eow = false;
+        }
+    }
+    static Node root = new Node();
+    public static void insert(String word){
+        for(int i=0;i<word.length();i++){//O(L)
+            int idx = word.charAt(i) - 'a';
+
+            if(root.children[idx]==null){
+                //add new Node
+                root.children[idx] = new Node();
+            }
+            if(i==word.length()-1){
+                root.children[idx].eow=true;
+            }
+            root = root.children[idx];
+        }
+    }
+    public static void main(String[] args) {
+        String words[] = {"the","a","there","their","any"};
+        for(int i=0;i<words.length;i++){
+            insert(words[i]);
+        }
+    }
+}
+
